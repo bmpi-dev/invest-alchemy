b9ea05931a4cb549faf2dc867e924b352c4da2da
madawei2699 <madawei2699@gmail.com>
2020-08-24 08:05:18 +0800

    done subscribe web page

--- web/client/pages/index.vue
+++ web/client/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <Row type="flex" justify="center">
-    <Col span="12">
+    <Col span="18">
       <div class="content">
           <div class="title"><h1>ETF双均线策略交易信号订阅</h1></div>
           <div>
-            <Input search enter-button="Subscribe" placeholder="Email" />
+            <Input class="email" v-model="email" placeholder="Email" />
+            <Button v-on:click="subscribe()" type="primary">Subscribe</Button>
+            <Alert class="message" v-if=this.subscribe_message_show type="success">订阅成功，请去邮箱确认订阅吧！(邮件标题为：AWS Notification - Subscription Confirmation)</Alert>
+            <Alert class="message" v-if=this.error_message_show type="error">{{this.error_message}}</Alert>
           </div>
       </div>
 
@@ -44,6 +47,14 @@
   .history_content ul {
     list-style: none;
   }
+
+  .email {
+    margin: 10px 0;
+  }
+
+  .message {
+    margin-top: 10px;
+  }
 </style>
 <script>
 export default {
@@ -54,6 +65,10 @@ export default {
   data() {
     return {
       day_urls: [],
+      email: null,
+      subscribe_message_show: false,
+      error_message: "",
+      error_message_show: false,
     }
   },
 
@@ -76,6 +91,40 @@ export default {
           arr.push(new Date(dt));
       }
       return arr;
+    },
+
+    subscribe: function() {
+      if (this.email != null && this.email != "") {
+        this.$Message.info({
+                content: "正在请求...",
+                duration: 3
+            });
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email: this.email })
+        };
+        fetch("https://fey17sm0g7.execute-api.us-east-1.amazonaws.com/dev/subscribe", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          const r = JSON.parse(data);
+          const body = JSON.parse(r.body);
+          if (body.code == 0) {
+            this.subscribe_message_show = true;
+            this.error_message_show = false;
+            this.error_message = "";
+          } else {
+            this.subscribe_message_show = false;
+            this.error_message_show = true;
+            this.error_message = body.message;
+          }
+        });
+      } else {
+        this.$Message.info({
+                content: "请填写邮箱！",
+                duration: 3
+            });
+      }
     }
   }
 };
