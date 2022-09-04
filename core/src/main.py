0ae14ab1b3d4ea0e21307b28c3de977c25e6dd91
Dawei Ma <madawei2699@gmail.com>
2022-09-04 23:32:03 +0800

    chore: update robot trader portfolio calculate time

--- core/src/main.py
+++ core/src/main.py
@@ -14,11 +14,14 @@ from trader.robot.dma_trader_v01 import DMATraderV01
 from trader.robot.dma_trader_v02 import DMATraderV02
 
 def robot_trader_portfolio_cal():
+    yesterday = datetime.strptime(TODAY_STR, TRADE_DATE_FORMAT_STR) - timedelta(1)
+    yesterday_str = the_day_before_trade_date.strftime(TRADE_DATE_FORMAT_STR)
+
     trader1 = DMATraderV01()
-    trader1.update_portfolios(TODAY_STR)
+    trader1.update_portfolios(yesterday_str)
 
     trader2 = DMATraderV02()
-    trader2.update_portfolios(TODAY_STR)
+    trader2.update_portfolios(yesterday_str)
 
 def startup():
     print('make sure local bash path exists...')
