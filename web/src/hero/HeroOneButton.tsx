171613bb2fbf7981cf22605d06d640453363c8d6
Dawei Ma <madawei2699@gmail.com>
2023-05-17 09:15:14 +0800

    Update HeroOneButton.tsx

--- web/src/hero/HeroOneButton.tsx
+++ web/src/hero/HeroOneButton.tsx
@@ -7,52 +7,6 @@ type IHeroOneButtonProps = {
 };
 
 const HeroOneButton = (props: IHeroOneButtonProps) => {
-  const [email, setEmail] = useState<string>('');
-  const [errorMsg, setErrorMsg] = useState<string>('');
-
-  let timeoutId: any;
-
-  const setErrMsg = (err: string, timeSecond = 2000) => {
-    if (timeoutId !== null) {
-      clearTimeout(timeoutId);
-    }
-    setErrorMsg(err);
-    timeoutId = setTimeout(() => {
-      setErrorMsg('');
-      timeoutId = null;
-    }, timeSecond);
-  };
-
-  const subscribeSNS = () => {
-    if (email != null && email !== '') {
-      setErrMsg('正在请求...');
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email }),
-      };
-      fetch(
-        'https://fey17sm0g7.execute-api.us-east-1.amazonaws.com/dev/subscribe',
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          const r = JSON.parse(data);
-          const body = JSON.parse(r.body);
-          if (body.code !== 0) {
-            setErrMsg(body.message);
-          } else {
-            setErrMsg(
-              '即将订阅成功，请在邮件里确认订阅（邮件标题：AWS Notifications），如果没收到，请在垃圾箱中检查！',
-              3000
-            );
-          }
-        });
-    } else {
-      setErrMsg('请填写邮箱！');
-    }
-  };
-
   return (
     <header className="text-center">
       <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
@@ -68,7 +22,7 @@ const HeroOneButton = (props: IHeroOneButtonProps) => {
             </h2>
 
             <p className="text-red-500 sm:mt-4">
-              付费请发邮件至 me@i365.tech 来信咨询
+              付费请发邮件至 <me@i365.tech> 来信咨询
             </p>
 
             <p className="text-gray-500 sm:mt-2">
