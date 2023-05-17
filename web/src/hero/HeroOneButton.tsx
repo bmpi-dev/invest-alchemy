e05bf3615c85ea386295316c3168949f7b070de9
Dawei Ma <madawei2699@gmail.com>
2023-05-17 08:56:39 +0800

    update web

--- web/src/hero/HeroOneButton.tsx
+++ web/src/hero/HeroOneButton.tsx
@@ -64,61 +64,16 @@ const HeroOneButton = (props: IHeroOneButtonProps) => {
         <div className="p-8 md:p-12 lg:px-16 lg:py-24">
           <div className="max-w-lg mx-auto text-center">
             <h2 className="text-2xl font-bold text-gray-500 md:text-3xl">
-              交易信号订阅
+              交易信号邮件提醒付费订阅
             </h2>
 
-            <p className="text-gray-500 sm:mt-4">
+            <p className="text-red-500 sm:mt-4">
+              付费请发邮件至 me@i365.tech 来信咨询
+            </p>
+
+            <p className="text-gray-500 sm:mt-2">
               目前仅支持双均线策略信号的订阅，未来会支持多种交易策略信号
             </p>
-          </div>
-
-          {errorMsg !== '' && (
-            <div
-              className="p-4 text-red-700 border rounded border-red-900/10 bg-red-50 mt-2"
-              role="alert"
-            >
-              <strong className="text-sm font-medium"> {errorMsg} </strong>
-            </div>
-          )}
-
-          <div className="max-w-xl mx-auto mt-8">
-            <form action="#" className="sm:gap-4 sm:flex">
-              <div className="sm:flex-1">
-                <label htmlFor="email" className="sr-only">
-                  Email
-                </label>
-
-                <input
-                  type="email"
-                  placeholder="邮箱"
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full p-3 text-gray-700 bg-white border-gray-200 transition rounded-md shadow-sm focus:ring focus:outline-none focus:ring-yellow-400 focus:border-white"
-                />
-              </div>
-
-              <button
-                type="button"
-                onClick={subscribeSNS}
-                className="flex items-center justify-center w-full px-5 py-3 mt-4 text-white transition rounded-md bg-rose-600 sm:mt-0 sm:w-auto group focus:outline-none focus:ring focus:ring-yellow-400"
-              >
-                <span className="text-sm font-medium"> 订阅 </span>
-
-                <svg
-                  className="w-5 h-5 ml-3"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </button>
-            </form>
           </div>
         </div>
       </aside>
